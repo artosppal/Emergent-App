f29ef8d9531c776e02f746332641f57b4825c56d
artosppal <artosppal@gmail.com>
2026-09-04 05:56:22 +0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1101,7 +1101,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://emergent-app-vert.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
